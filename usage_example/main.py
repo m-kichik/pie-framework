473475c6c4a6ff2bcbc906e2afe2aec632a97cc6
Vladimir Konoplyuk <konoplyuk.v@yandex.ru>
2023-11-26 15:24:46 +0300

    minor fixes

--- usage_example/main.py
+++ usage_example/main.py
@@ -10,10 +10,9 @@ def post_hello_world(json_data, **query_params):
 
 if __name__ == "__main__":
     app = Application()
-    server = Server()
+    app.register_route("GET", "/hello/world", get_hello_world)
+    app.register_route("POST", "/hello/world", post_hello_world)
 
-    app.register_route("/hello/world", "GET", get_hello_world)
-    app.register_route("/hello/world", "POST", post_hello_world)
-
+    server = Server("localhost", 8080, app)
     server.run()
     
