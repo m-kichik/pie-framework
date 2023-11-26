e44c6ebadecd8b660f5f5f319795556d1269fbf0
m-kichik <kichik.mg@phystech.edu>
2023-11-26 14:36:40 +0300

    handle_request v0.2

--- src/app.py
+++ src/app.py
@@ -1,7 +1,17 @@
 from typing import Dict
 from urllib.parse import urlparse
 
+from responce import Response
 
+def singleton(class_):
+    instances = {}
+    def getinstance(*args, **kwargs):
+        if class_ not in instances:
+            instances[class_] = class_(*args, **kwargs)
+        return instances[class_]
+    return getinstance
+
+@singleton
 class Application:
     def __init__(self):
         self.routes: Dict[str, callable] = {}
@@ -16,11 +26,14 @@ class Application:
 
     async def handle_request(self, method:str, path:str, params:dict) -> None :
         if (method, path) not in self.routes:
-            return 'HTTP/2 404 Not Found'
+            return Response(status=404, reason='Not Found')
         
         handler = self.routes[(method, path)]
-
         
         response = handler(**params)
-        
-
+        if isinstance(response, dict):
+            return Response(status=response.get('status'),
+                            reason=response.get('reason'),
+                            headers=response.get('headers'),
+                            body=response.get('body'))
+        # elif isinstance(response)
